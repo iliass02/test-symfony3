81533a8893ad77f9cdc3fe08dbdd8642f0cc6042
iliass <iliass.marchoud@gmail.com>
2016-06-25 16:50:35 +0200

    add assetic

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,8 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
             new MainBundle\MainBundle(),
+
+            new Symfony\Bundle\AsseticBundle\AsseticBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
